cb6aae6616b868a49169f45680090b21d9dc7475
Shurr <zerodreamer62@gmail.com>
2026-08-13 12:00:00 +0000

    Complete Day 4 codes

---
+++ Day4_Q7.c
@@ -0,0 +1,12 @@
+// Write a program to swap two numbers without using a third variable.
+
+#include <stdio.h>
+int main(){
+int a,b;
+printf("Enter numbers: ");
+scanf("%d %d", &a, &b);
+printf("Swapped digits are %d",b);
+printf(" and %d\n",a);
+return 0;}
+
+
