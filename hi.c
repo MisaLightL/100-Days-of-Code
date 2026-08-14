9666abc477e9d686261ca070ee17ffb29debf22a
Shurr <zerodreamer62@gmail.com>
2026-08-14 05:51:29 +0000

    Add hi.c

---
+++ hi.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+int a;
+int b;
+printf("First number: %d",a);
+scanf("%d",&a);
+printf("Second number: %d",b);
+scanf("%d",&b);
+int c=a+b;
+printf("The sum is %d\n",c);
+return 0;
+}
