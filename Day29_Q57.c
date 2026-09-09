0021a2f725b0dcb2f7c2bddacd96381e2863e67b
Shurr <zerodreamer62@gmail.com>
2026-09-09 05:02:27 +0000

    Add Day 29 solutions

---
+++ Day29_Q57.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, i;
+    int sum = 0;
+
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        sum = sum + a[i];
+    }
+
+    printf("Sum = %d", sum);
+
+    return 0;
+}
