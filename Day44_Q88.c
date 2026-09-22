5acf8a51a3b63c40d99169f920916e58f38a643b
Shurr <zerodreamer62@gmail.com>
2026-09-22 16:51:36 +0000

    Day 44 solutions

---
+++ Day44_Q88.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int i = 0;
+
+    printf("Enter a string: ");
+    scanf("%[^\n]", str);
+
+    while(str[i] != '\0')
+    {
+        if(str[i] == ' ')
+        {
+            str[i] = '-';
+        }
+
+        i++;
+    }
+
+    printf("%s", str);
+
+    return 0;
+}
