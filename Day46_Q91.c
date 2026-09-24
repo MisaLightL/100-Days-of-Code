b341acc39aa230ea098c2cfb8e262c8082852400
Shurr <zerodreamer62@gmail.com>
2026-09-24 18:02:53 +0000

    Day 46 solutions

---
+++ Day46_Q91.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int i = 0;
+
+    printf("Enter a string: ");
+    scanf("%[^\n]", str);
+
+    while(str[i] != '\0')
+    {
+        if(str[i]=='a' || str[i]=='e' || str[i]=='i' ||
+           str[i]=='o' || str[i]=='u' ||
+           str[i]=='A' || str[i]=='E' || str[i]=='I' ||
+           str[i]=='O' || str[i]=='U')
+        {
+            /* Do nothing */
+        }
+        else
+        {
+            printf("%c", str[i]);
+        }
+
+        i++;
+    }
+
+    return 0;
+}
