2483cdeec7e4c694fa0d7b1051c82949d9079053
Shurr <zerodreamer62@gmail.com>
2026-09-25 18:26:56 +0000

    Day 47 solutions

---
+++ Day47_Q93.c
@@ -0,0 +1,48 @@
+// Q93
+// ANSWER:
+
+#include <stdio.h>
+
+int main()
+{
+    char str1[100], str2[100];
+    int freq1[26] = {0};
+    int freq2[26] = {0};
+    int i, anagram = 1;
+
+    printf("Enter first string: ");
+    scanf("%s", str1);
+
+    printf("Enter second string: ");
+    scanf("%s", str2);
+
+    i = 0;
+    while(str1[i] != '\0')
+    {
+        freq1[str1[i] - 'a']++;
+        i++;
+    }
+
+    i = 0;
+    while(str2[i] != '\0')
+    {
+        freq2[str2[i] - 'a']++;
+        i++;
+    }
+
+    for(i = 0; i < 26; i++)
+    {
+        if(freq1[i] != freq2[i])
+        {
+            anagram = 0;
+            break;
+        }
+    }
+
+    if(anagram)
+        printf("Anagrams");
+    else
+        printf("Not anagrams");
+
+    return 0;
+}
